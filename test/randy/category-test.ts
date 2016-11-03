97235693c6a78ee1437c279fe114e43f7ac1a42b
Randy Sagum <randy.sagum@yahoo.com>
2016-11-03 16:29:43 +0800

    added test get() on unit test

--- test/randy/category-test.ts
+++ test/randy/category-test.ts
@@ -1,8 +1,8 @@
 import { Test as t } from '../test';
 import { Post, PostData } from '../../post';
 import { Category, CategoryData } from '../../category';
-import * as firebase from 'firebase';
-import { Database } from '../fireframe/database';
+//import * as firebase from 'firebase';
+//import { Database } from '../fireframe/database';
 export class CategoryRandyTest {
     post: Post; // Post instance with temporary post path.
     posts: Array<PostData>;
@@ -23,7 +23,9 @@ export class CategoryRandyTest {
             () => this.testSets(
                 () =>  this.createTest(          
                     () => this.createSameCategoryTest( 
-                       ()=> this.testgets(callback)
+                       () => this.testgets(
+                           () => this.testget(callback)
+                        )
                     )
                 ) 
             )
@@ -78,10 +80,37 @@ export class CategoryRandyTest {
             }, e => {
                 t.fail("Error creating category :: "+ e )          
             });   
-
-            
-
     }
+
+
+    
+    testget(callback){
+        this. category.destroy();    
+        this.category.id("Two").name("Four").title("Six")
+           .create( s => {                    
+                if ( s ) t.fail('Test gets() :: fail to create a category: ' + s );           
+                else { t.pass("Success on creating category")   
+                    this.category.get( "Two", 
+                    s =>{ 
+                          if(s){
+                              if(s.name === "Four") t.pass("Test get() :: success on getting correct data");  
+                          }else{
+                              t.fail("Test get():: should return object instead, but it returns "+ s);
+                          }    
+
+                          callback();                     
+                    },
+                    e =>{
+                        t.fail("Test get():: fail to get data on server")
+                         callback(); 
+                    });                           
+                }                        
+            }, e => {
+                t.fail("Error creating category :: "+ e )   
+                 callback();        
+            });   
+    }
+
 
     testSets( callback ) {
 
